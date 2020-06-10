c2a0279927bf7c32b5f2507905ad146cff742770
Dennis Keil <dennis.keil10@gmail.com>
2020-06-11 01:40:35 +0200

    Cache state when fast refreshing

---
+++ src/redux/statecache.ts
@@ -0,0 +1,12 @@
+
+let mycache = [];
+
+export function setcache(value: any) {
+    mycache = value;
+    console.log('setcache', value);
+}
+
+export function getcache() {
+    console.log('getcache', mycache);
+    return mycache;
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux'
 import notesReducer from './notesApp'
+import {getcache, setcache} from "./statecache";
 
-const store = createStore(notesReducer)
+const store = createStore(notesReducer, getcache())
+
+store.subscribe(() => setcache(store.getState()));
 
 export default store
